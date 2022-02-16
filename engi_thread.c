779f53d245e74f6ef87cd34c36f2e10199158748
Malformed C <enderman08@gmail.com>
2022-02-16 18:21:26 +0300

    new functioins

--- engi_thread.c
+++ engi_thread.c
@@ -5,36 +5,94 @@
 
 int engi_thread_init(engi_thread_t *self)
 {
-	self->attr = NULL;
+	pthread_attr_init(&self->attr);
+
+	pthread_mutexattr_init(&self->mutex_attr);
+	pthread_mutexattr_settype(&self->mutex_attr, PTHREAD_MUTEX_ERRORCHECK);
+	pthread_mutexattr_setrobust(&self->mutex_attr, PTHREAD_MUTEX_ROBUST);
+
+	pthread_mutex_init(&self->mutex, &self->mutex_attr);
+
+	pthread_condattr_init(&self->cond_attr);
+	pthread_cond_init(&self->cond, &self->cond_attr);
 
 	return 0;
 }
 
-int engi_thread_create(engi_thread_t *self, engi_task_t *task)
+int engi_thread_create(engi_thread_t *self)
 {
-	int res = 0;
+	int ret = 0;
 
-	res = pthread_create(&self->thread, self->attr, task->func, task->args);
+	engi_task_t *task = &self->task;
 
-	return res;
+	ret = pthread_create(&self->thread, &self->attr, task->func, task->args);
+
+	return ret;
 }
 
 int engi_thread_join(engi_thread_t *self)
 {
-	int res = 0;
+	int ret = 0;
 
-	res = pthread_join(self->thread, &self->retval);
+	ret = pthread_join(self->thread, &self->retval);
 
-	return res;
+	return ret;
 }
 
-engi_task_t * engi_task_init(engi_task_t *task, void * func(void *), void *args)
+void engi_thread_exit(void *retptr)
+{
+	pthread_exit(retptr);
+}
+
+int engi_task_init(engi_task_t *task, void * func(void *), void *args)
 {
 	task->func = func;
 	task->args = args;
-	return task;
+	
+	return 0;
 }
 
+int engi_mutex_lock(engi_thread_t *self)
+{
+	int ret = 0;
 
+	ret = pthread_mutex_lock(&self->mutex);
 
+	return ret;
+}
 
+int engi_mutex_unlock(engi_thread_t *self)
+{
+	int ret = 0;
+
+	ret = pthread_mutex_unlock(&self->mutex);
+
+	return ret;
+}
+
+int engi_cond_signal(engi_thread_t *self)
+{
+	int ret = 0;
+
+	ret = pthread_cond_signal(&self->cond);
+
+	return ret;
+}
+
+int engi_cond_broadcast(engi_thread_t *self)
+{
+	int ret = 0;
+
+	ret = pthread_cond_broadcast(&self->cond);
+
+	return ret;
+}
+
+int engi_cond_wait(engi_thread_t *self)
+{
+	int ret = 0;
+
+	ret = pthread_cond_wait(&self->cond, &self->mutex);
+
+	return ret;
+}
--- engi_thread.h
+++ engi_thread.h
@@ -11,16 +11,31 @@ typedef struct engi_task_s
 typedef struct engi_thread_s
 {
 	pthread_t thread;
-	pthread_attr_t *attr;
+	pthread_attr_t attr;
+	pthread_mutex_t mutex;
+	pthread_mutexattr_t mutex_attr;
+	pthread_cond_t cond;
+	pthread_condattr_t cond_attr;
 	void *retval;
 	engi_task_t task;
 } engi_thread_t;
 
-engi_task_t * engi_task_init(engi_task_t *task, void * func(void *), void *args);
-
 int engi_thread_init(engi_thread_t *self);
 
-int engi_thread_create(engi_thread_t *self, engi_task_t *task);
+int engi_thread_create(engi_thread_t *self);
 
 int engi_thread_join(engi_thread_t *self);
 
+void engi_thread_exit(void *retptr) __attribute__ ((noreturn));
+
+int engi_task_init(engi_task_t *task, void * func(void *), void *args);
+
+int engi_mutex_lock(engi_thread_t *self);
+
+int engi_mutex_unlock(engi_thread_t *self);
+
+int engi_cond_signal(engi_thread_t *self);
+
+int engi_cond_broadcast(engi_thread_t *self);
+
+int engi_cond_wait(engi_thread_t *self);
